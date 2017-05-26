63248529f3614008be606430151a487b4b392885
Triyo Ifanto <Triyo_I@mitrais.com>
2017-05-26 16:02:08 +0800

    iniziate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+
+
+ReactDOM.render(
+    <div>
+        <h1>Hello word</h1>
+    </div>
+    , document.getElementById('root'))
+registerServiceWorker();
