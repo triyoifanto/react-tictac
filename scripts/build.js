18e1e1439c5989ef1cbdb0787ad6fc0e363cf7a0
Triyo Ifanto <Triyo_I@mitrais.com>
2017-05-29 13:25:00 +0800

    first heroku commit

--- scripts/build.js
+++ scripts/build.js
@@ -2,6 +2,7 @@
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.NODE_ENV = 'production';
+process.env.GRAPHQL_ENDPOINT='https://api.graph.cool/relay/v1/cj35kmpw4l9st0145ews87lmn';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
